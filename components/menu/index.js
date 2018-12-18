c9f8d52091f15674847d2204a2298b764b254251
Mélanie Gaubert <melaniegaubert@MacBook-Pro-de-Melanie.local>
2018-12-18 13:10:18 +0100

    first commit

--- components/menu/index.js
+++ components/menu/index.js
@@ -1,12 +1,19 @@
 function toggleNav() {
     const element = document.querySelector("nav");
+    //const fermer = document.querySelector("fermer");
+    //const ouvert = document.querySelector("ouvert");
     const elementHeight = element.scrollHeight;
     var isCollapsed = element.getAttribute('data-collapsed') === 'true';
     if (isCollapsed) {
       element.style.height = elementHeight + "px";
       element.setAttribute("data-collapsed", "false");
+      //fermer.style.display = "none";
+      //ouvert.style.display = "block";
+
     } else {
-          element.style.height = "0px";
+      element.style.height = "0px";
       element.setAttribute("data-collapsed", "true");
+      //fermer.style.display = "block";
+      //ouvert.style.display = "none";
     }
   }
