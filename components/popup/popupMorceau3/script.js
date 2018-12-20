7c6f083d6d6cbe78a24c14bb59114d67956d6cc2
Solène Ramis <soleneramis@MacBook-Pro-de-Solene-2.local>
2018-12-21 00:50:32 +0100

    add all pop up

---
+++ components/popup/popupMorceau3/script.js
@@ -0,0 +1,14 @@
+document.querySelector('.pop-up').style.visibility = "hidden";
+document.querySelector('.pop-up-container').style.visibility = "hidden";
+
+ document.querySelector('.pop-up-button').addEventListener("click",function () {
+    document.querySelector('.pop-up-container').style.visibility = "visible";
+    document.querySelector('.pop-up').style.visibility = "visible";
+    document.querySelector('.pop-up-button').style.visibility = "hidden";
+});
+
+document.querySelector('.cross').addEventListener("click",function () {
+    document.querySelector('.pop-up-container').style.visibility = "hidden";
+    document.querySelector('.pop-up').style.visibility = "hidden";
+    document.querySelector('.pop-up-button').style.visibility = "visible";
+});
